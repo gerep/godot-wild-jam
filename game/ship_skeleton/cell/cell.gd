5f5ebc43768b90d836e269061f3a8234733d34de
Overkane <overkanex@gmail.com>
2026-06-15 01:14:33 +0500

    Connect cells to each other. Make cells regenerate on destruction

--- game/ship_skeleton/cell/cell.gd
+++ game/ship_skeleton/cell/cell.gd
@@ -1,11 +1,16 @@
 class_name Cell
 extends Node2D
 
+signal restored
 signal destroyed
 
 # TODO. On core destroyed destroye owner too.
 @export var is_core := false
 @export var health: float
+@export var core_parents: Array[Cell]
+@export var repair_rate: float
+
+var is_destroyed := false
 
 var _related_movemenet_collision_shape: CollisionShape2D
 
@@ -18,6 +23,24 @@ func _ready() -> void:
 	health_component.health_depleted.connect(_on_health_depleted)
 
 	health_component.set_health(health)
+
+func _physics_process(delta: float) -> void:
+	if is_destroyed:
+		var can_restore := false
+		for core_parent: Cell in core_parents:
+			if not core_parent.is_destroyed:
+				can_restore = true
+
+		if can_restore:
+			health_component.current_health += repair_rate * delta
+			if health_component.current_health >= health:
+				restored.emit()
+				_related_movemenet_collision_shape.disabled = false
+				hurtbox_2d.monitorable = true
+				modulate.a = 1.0
+				is_destroyed = false
+		elif health_component.current_health > 0:
+			health_component.current_health = 0
 
 
 func get_hurtbox_collision_shape() -> CollisionShape2D:
@@ -34,8 +57,10 @@ func activate_hurtbox_layer(layer: int) -> void:
 
 func _on_health_depleted() -> void:
 	destroyed.emit()
-	_related_movemenet_collision_shape.queue_free()
-	queue_free()
+	_related_movemenet_collision_shape.disabled = true
+	modulate.a = 0.5
+	hurtbox_2d.monitorable = false
+	is_destroyed = true
 
 func _on_hit(hitbox: ComponentHitbox2D) -> void:
 	health_component.take_damage(hitbox.damage)
--- game/ship_skeleton/ship_skeleton.gd
+++ game/ship_skeleton/ship_skeleton.gd
@@ -1,10 +1,27 @@
+@tool
 class_name ShipSkeleton
 extends Node2D
 
 signal destroyed
 
+const CELL_CONNECTOR = preload("uid://cdeg2ih3g8ys4")
+
+@export_tool_button("Calculate core connections")
+
+var calculate_core_connections_action: Callable = calculate_core_connections
+
+@onready var cell_connectors: Node2D = %CellConnectors
+
+
 func _ready() -> void:
-	for cell: Cell in get_children():
+	if Engine.is_editor_hint():
+		return
+
+	for child in get_children():
+		if not child is Cell:
+			continue
+		var cell = child as Cell
+
 		# Gives parent collision shapes of all cells. Since the movement logic is located in owner node.
 		var hurtbox_shape := cell.get_hurtbox_collision_shape()
 		var collision_shape := hurtbox_shape.duplicate()
@@ -23,3 +40,57 @@ func _ready() -> void:
 			cell.destroyed.connect(func():
 				destroyed.emit()
 			)
+
+
+# TOOL. Connects cells between each other depending on position relative to the core.
+func calculate_core_connections():
+	for child in cell_connectors.get_children():
+		child.queue_free()
+
+	var core: Cell
+
+	for cell: Cell in get_children():
+		if cell.is_core:
+			core = cell
+			break
+
+	fill_core_connections(core)
+
+func fill_core_connections(cell: Cell) -> void:
+	var shape = cell.get_node("Hurtbox2D/CollisionShape2D").shape.duplicate()
+	var distance_check_mult := 1.25
+
+	# CRITICAL. Assuming we only use capsule shapes for cells
+	assert(shape is CapsuleShape2D, "Core is not found")
+
+	shape = shape as CapsuleShape2D
+
+	shape.height *= distance_check_mult
+	shape.radius *= distance_check_mult
+
+	var dss := get_world_2d().direct_space_state
+	var query: PhysicsShapeQueryParameters2D = PhysicsShapeQueryParameters2D.new()
+	query.collide_with_areas = true
+	query.collision_mask = pow(2, CollisionLayers.CORE_CONNECTION_CHECKER - 1)
+	query.shape = shape
+	query.transform = cell.get_transform()
+	query.exclude = [cell.get_node("Hurtbox2D").get_rid()]
+
+	var results = dss.intersect_shape(query)
+	for result in results:
+		var colliding_cell: Cell = result.collider.owner
+		if cell.core_parents.has(colliding_cell):
+			continue
+
+		var cell_connector: Line2D = CELL_CONNECTOR.instantiate()
+		var my_array: Array
+		my_array.append(cell.position)
+		my_array.append(colliding_cell.position)
+		cell_connector.points = my_array
+
+		cell_connectors.add_child(cell_connector)
+
+		colliding_cell.core_parents.clear()
+		colliding_cell.core_parents.append(cell)
+
+		fill_core_connections(colliding_cell)
