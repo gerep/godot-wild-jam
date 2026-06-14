3724bc2b8d511f8708b0b34c5263ab1ee69a3d20
Overkane <overkanex@gmail.com>
2026-06-14 20:55:00 +0500

    Fix owner of bullet with new ship system

--- game/player/base_player_turret.gd
+++ game/player/base_player_turret.gd
@@ -1,3 +1,4 @@
+class_name BasePlayerTurret
 extends Node2D
 
 @onready var shooting_pos: Marker2D = $ShootingPos
@@ -9,7 +10,8 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"shoot"):
 		var direction := shooting_pos.global_position.direction_to(get_global_mouse_position())
-		Projectile.create(owner, shooting_pos.global_position, direction, Projectiles.SIMPLE_BULLET)
+		# HACK. Owner will be ship skeleton. But we need owner of ship skeleton.
+		Projectile.create(owner.owner, shooting_pos.global_position, direction, Projectiles.SIMPLE_BULLET)
 
 	if event is InputEventMouseMotion:
 		look_at(get_global_mouse_position())
