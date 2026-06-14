9635e73ff1751ce912493c4ac2256a717c6835d2
gerep <181501+gerep@users.noreply.github.com>
2026-06-14 12:14:43 -0300

    arena bridge movement

---
+++ game/arena_bridge/arena_bridge.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var start_trigger_area_2d: Area2D = $StartTriggerArea2D
+@onready var end_trigger_area_2d: Area2D = $EndTriggerArea2D
+@onready var start_exit_marker_2d: Marker2D = $StartExitMarker2D
+@onready var end_exit_marker_2d_2: Marker2D = $EndExitMarker2D2
+
+
+func _ready() -> void:
+	start_trigger_area_2d.body_entered.connect(_on_start_trigger_body_entered)
+	end_trigger_area_2d.body_entered.connect(_on_end_trigger_body_entered)
+
+
+func _on_start_trigger_body_entered(body: Node2D) -> void:
+	var player: Player = body as Player
+	if player == null or player.is_auto_moving:
+		return
+
+	player.auto_move_to(end_exit_marker_2d_2.global_position)
+
+
+func _on_end_trigger_body_entered(body: Node2D) -> void:
+	var player: Player = body as Player
+	if player == null or player.is_auto_moving:
+		return
+
+	player.auto_move_to(start_exit_marker_2d.global_position)
--- game/player/player.gd
+++ game/player/player.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+const TRANSPORTING_SPEED = 1000.0
+const AUTO_MOVE_STOP_DISTANCE = 4.0
+
+var is_auto_moving: bool = false
+var auto_move_target: Vector2
+
 @onready var _hurtbox_2d: ComponentHurtbox2D = %Hurtbox2D
 @onready var _experience: ComponentExperience = %Experience
 
@@ -10,12 +16,34 @@ const SPEED = 300.0
 func _ready() -> void:
 	_hurtbox_2d.got_hit.connect(_on_got_hit)
 
+
 func _unhandled_input(event: InputEvent) -> void:
+	if is_auto_moving:
+		return
+
 	if event.is_action_pressed(&"shoot"):
 		var direction := global_position.direction_to(get_global_mouse_position())
 		Projectile.create(self, global_position, direction, Projectiles.SIMPLE_BULLET)
 
+
 func _physics_process(delta: float) -> void:
+	if is_auto_moving:
+		var distance_to_target := global_position.distance_to(auto_move_target)
+		# This is a way to avoid the player from overshoothing the `auto_move_target` when the 
+		# transport speed is too high.
+		var max_travel_distance := TRANSPORTING_SPEED * delta
+
+		if distance_to_target <= max(AUTO_MOVE_STOP_DISTANCE, max_travel_distance):
+			global_position = auto_move_target
+			velocity = Vector2.ZERO
+			is_auto_moving = false
+		else:
+			var auto_move_direction := global_position.direction_to(auto_move_target)
+			velocity = auto_move_direction * TRANSPORTING_SPEED
+			move_and_slide()
+
+		return
+
 	var direction := Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
 	velocity = direction * SPEED
 
@@ -26,3 +54,9 @@ func _on_got_hit(hitbox: ComponentHitbox2D) -> void:
 	if hitbox.owner is XpOrb:
 		var xp_orb := hitbox.owner as XpOrb
 		_experience.add_xp(xp_orb.get_xp())
+
+
+func auto_move_to(pos: Vector2) -> void:
+	is_auto_moving = true
+	auto_move_target = pos
+	velocity = Vector2.ZERO
