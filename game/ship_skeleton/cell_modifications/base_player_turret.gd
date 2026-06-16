8d07dfcce0cf9649879f74731e1cbd42ad62409d
Overkane <overkanex@gmail.com>
2026-06-17 03:50:39 +0500

    Make player's turret shoot straight

--- game/ship_skeleton/cell_modifications/base_player_turret.gd
+++ game/ship_skeleton/cell_modifications/base_player_turret.gd
@@ -6,9 +6,12 @@ extends Node2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"shoot"):
-		var direction := shooting_pos.global_position.direction_to(get_global_mouse_position())
+		# NOTE. For player will shot straight for now. For AI perhaps could rotate turrets.
+		#var direction := shooting_pos.global_position.direction_to(get_global_mouse_position())
+		var direction := Vector2.RIGHT.rotated(global_rotation)
 		# HACK. Owner will be ship skeleton. But we need owner of ship skeleton.
 		Projectile.create(owner.owner, shooting_pos.global_position, direction, Projectiles.SIMPLE_BULLET)
 
-	if event is InputEventMouseMotion:
-		look_at(get_global_mouse_position())
+	# NOTE. Disable turrent rotation for now.
+	#if event is InputEventMouseMotion:
+		#look_at(get_global_mouse_position())
