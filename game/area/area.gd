68618f6272212730122e909ce4795becc2f6a666
gerep <181501+gerep@users.noreply.github.com>
2026-06-18 10:18:38 -0300

    map creation

--- game/area/area.gd
+++ game/area/area.gd
@@ -3,13 +3,75 @@ extends Node2D
 
 @onready var player_detector: Area2D = $PlayerDetector
 
+@onready var west_creation_area: Area2D = $AreaCreation/WestCreationArea
+@onready var west_transport_area: Area2D = $Transport/WestTransportArea
+@onready var west_marker: Marker2D = $Markers/WestMarker
+
+@onready var east_creation_area: Area2D = $AreaCreation/EastCreationArea
+@onready var east_transport_area: Area2D = $Transport/EastTransportArea
+@onready var east_marker: Marker2D = $Markers/EastMarker
+
+@onready var north_creation_area: Area2D = $AreaCreation/NorthCreationArea
+@onready var north_transport_area: Area2D = $Transport/NorthTransportArea
+@onready var north_marker: Marker2D = $Markers/NorthMarker
+
+@onready var south_creation_area: Area2D = $AreaCreation/SouthCreationArea
+@onready var south_transport_area: Area2D = $Transport/SouthTransportArea
+@onready var south_marker: Marker2D = $Markers/SouthMarker
+
+signal new_area_requested(area: ArenaArea, side: StringName)
+signal transport_requested(area: ArenaArea, side: StringName)
+
+var grid_position := Vector2i.ZERO
+
+
 func _ready() -> void:
-	player_detector.body_entered.connect(_on_player_detector_body_entered)
+	west_creation_area.body_entered.connect(_on_creation_area_body_entered.bind(&"west", west_creation_area))
+	west_transport_area.body_entered.connect(_on_transport_area_body_entered.bind(&"west"))
+
+	east_creation_area.body_entered.connect(_on_creation_area_body_entered.bind(&"east", east_creation_area))
+	east_transport_area.body_entered.connect(_on_transport_area_body_entered.bind(&"east"))
+
+	north_creation_area.body_entered.connect(_on_creation_area_body_entered.bind(&"north", north_creation_area))
+	north_transport_area.body_entered.connect(_on_transport_area_body_entered.bind(&"north"))
+
+	south_creation_area.body_entered.connect(_on_creation_area_body_entered.bind(&"south", south_creation_area))
+	south_transport_area.body_entered.connect(_on_transport_area_body_entered.bind(&"south"))
 
 
-func _on_player_detector_body_entered(body: Node2D) -> void:
-	var player := body as Player
-	if player == null:
-		return
+func _on_transport_area_body_entered(_body: Node2D, side: StringName) -> void:
+	transport_requested.emit(self, side)
 
-	player.current_area = self
+
+func get_marker_for_side(side: StringName) -> Vector2:
+	match side:
+		&"north":
+			return north_marker.global_position
+		&"south":
+			return south_marker.global_position
+		&"east":
+			return east_marker.global_position
+		&"west":
+			return west_marker.global_position
+
+	return Vector2.ZERO
+
+
+# Once created, we don't need to monitor the area anymore.
+func _on_creation_area_body_entered(_body: Node2D, side: StringName, area: Area2D) -> void:
+	area.set_deferred(&"monitoring", false)
+	new_area_requested.emit(self, side)
+
+
+# When this area is created beside another one, disable the creation trigger that points back to the
+# already-existing neighbor.
+func disable_area_creation_on_side(side: StringName) -> void:
+	match side:
+		&"north":
+			south_creation_area.set_deferred(&"monitoring", false)
+		&"south":
+			north_creation_area.set_deferred(&"monitoring", false)
+		&"east":
+			west_creation_area.set_deferred(&"monitoring", false)
+		&"west":
+			east_creation_area.set_deferred(&"monitoring", false)
--- game/game_area/game_area.gd
+++ game/game_area/game_area.gd
@@ -1,2 +1,83 @@
 class_name GameArea
 extends Node2D
+
+const ROOM_SPACING = 3000
+const AREA = preload("uid://b8s7r2s04sqw3")
+
+@onready var area: ArenaArea = $Area
+@onready var player: Player = $Player
+
+var areas: Dictionary[Vector2i, ArenaArea] = {}
+
+
+func _ready() -> void:
+	player.current_area = area
+	_register_area(area)
+
+
+func _on_area_transport_requested(source_area: ArenaArea, side: StringName) -> void:
+	if player.is_auto_moving:
+		return
+
+	var target_grid_position := source_area.grid_position + _get_direction(side)
+	var target_area: ArenaArea = areas.get(target_grid_position)
+
+	if target_area == null:
+		return
+
+	var marker := target_area.get_marker_for_side(_get_opposite_side(side))
+	player.current_area = target_area
+	player.auto_move_to(marker)
+
+
+func _get_opposite_side(side: StringName) -> StringName:
+	match side:
+		&"west":
+			return &"east"
+		&"east":
+			return &"west"
+		&"north":
+			return &"south"
+		&"south":
+			return &"north"
+
+	return &""
+
+
+func _deferred_new_area(source_area: ArenaArea, side: StringName) -> void:
+	_on_new_area_requested.call_deferred(source_area, side)
+
+
+func _register_area(area_to_register: ArenaArea) -> void:
+	areas[area_to_register.grid_position] = area_to_register
+	area_to_register.transport_requested.connect(_on_area_transport_requested)
+	area_to_register.new_area_requested.connect(_deferred_new_area)
+
+
+func _on_new_area_requested(source_area: ArenaArea, side: StringName) -> void:
+	var target_grid_position := source_area.grid_position + _get_direction(side)
+	if areas.has(target_grid_position):
+		return
+
+	var new_area: ArenaArea = AREA.instantiate()
+	new_area.grid_position = target_grid_position
+	new_area.position = Vector2(target_grid_position) * ROOM_SPACING
+
+	add_child(new_area)
+
+	new_area.disable_area_creation_on_side(side)
+	_register_area(new_area)
+
+
+func _get_direction(side: StringName) -> Vector2i:
+	match side:
+		&"north":
+			return Vector2i.UP
+		&"south":
+			return Vector2i.DOWN
+		&"east":
+			return Vector2i.RIGHT
+		&"west":
+			return Vector2i.LEFT
+
+	return Vector2i.ZERO
