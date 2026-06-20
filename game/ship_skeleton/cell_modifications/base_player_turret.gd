350d793d2a8fc7a5ab537e9b38cfc3d629e01533
Overkane <overkanex@gmail.com>
2026-06-20 16:50:06 +0500

    Add shooting cooldown for turret

--- game/ship_skeleton/cell_modifications/base_player_turret.gd
+++ game/ship_skeleton/cell_modifications/base_player_turret.gd
@@ -1,11 +1,23 @@
 class_name BasePlayerTurret
 extends Node2D
 
+@export var _attack_cooldown := 1.0
+
+var _can_shoot := true
+
 @onready var shooting_pos: Marker2D = %ShootingPos
+@onready var attack_cooldown_timer: Timer = %AttackCooldownTimer
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed(&"shoot"):
+func _ready() -> void:
+	attack_cooldown_timer.timeout.connect(func():
+		_can_shoot = true
+	)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed(&"shoot") and _can_shoot:
+		_can_shoot = false
+		attack_cooldown_timer.start(_attack_cooldown)
 		# NOTE. For player will shot straight for now. For AI perhaps could rotate turrets.
 		#var direction := shooting_pos.global_position.direction_to(get_global_mouse_position())
 		var direction := Vector2.RIGHT.rotated(global_rotation)
